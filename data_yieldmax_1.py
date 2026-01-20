c3e2b20464491b95be7642bab662da1e1e969360
gla512356 <gla512356@gmail.com>
2026-01-20 21:07:03 +0900

    Update data_yieldmax_1.py

--- data_yieldmax_1.py
+++ data_yieldmax_1.py
@@ -7,23 +7,23 @@ def get_data():
         "theme_color": ["#1a2980", "#26d0ce"], 
 
         "schedule": {
-            "buy_limit": "1/14(수) 06:00", 
-            "ex_date": "1/14(수)",
-            "pay_date": "1/15(목)" 
+            "buy_limit": "1/21(수) 06:00", 
+            "ex_date": "1/21(수)",
+            "pay_date": "1/22(목)" 
         },
 
         "tickers": {
-            'CHPY': {'div': 0.5196, 'rate': 45.24, 'sec': 0.00, 'roc': 100.00, 'name': 'Semiconductor Portfolio'},
-            'FEAT': {'div': 0.2494, 'rate': 51.50, 'sec': 79.10, 'roc': 61.86, 'name': 'Dorsey Wright Featured 5'},
-            'FIVY': {'div': 0.1765, 'rate': 29.55, 'sec': 42.30, 'roc': 61.71, 'name': 'Dorsey Wright Hybrid 5'},
-            'GPTY': {'div': 0.3365, 'rate': 40.50, 'sec': 0.00, 'roc': 72.24, 'name': 'AI & Tech Portfolio'},
-            'LFGY': {'div': 0.2835, 'rate': 55.95, 'sec': 3.47, 'roc': 89.24, 'name': 'Crypto Industry & Tech'},
-            'QDTY': {'div': 0.2416, 'rate': 28.62, 'sec': 0.23, 'roc': 100.00, 'name': 'Nasdaq 100 0DTE'},
-            'RDTY': {'div': 0.2741, 'rate': 35.10, 'sec': 1.48, 'roc': 100.00, 'name': 'R2000 0DTE'},
-            'SDTY': {'div': 0.1805, 'rate': 20.80, 'sec': 0.03, 'roc': 81.60, 'name': 'S&P 500 0DTE'},
-            'SLTY': {'div': 0.3580, 'rate': 60.56, 'sec': 1.81, 'roc': 95.63, 'name': 'Ultra Short Option'},
-            'ULTY': {'div': 0.5186, 'rate': 70.83, 'sec': 0.00, 'roc': 100.00, 'name': 'Ultra Option Strategy'},
-            'YMAG': {'div': 0.0634, 'rate': 23.25, 'sec': 64.33, 'roc': 0.30, 'name': 'Magnificent 7 Fund'},
-            'YMAX': {'div': 0.0816, 'rate': 41.82, 'sec': 94.58, 'roc': 41.70, 'name': 'Universe Fund'},
+            'CHPY': {'div': 0.5267, 'rate': 45.51, 'sec': 0.00, 'roc': 100.00, 'name': 'Semiconductor Portfolio'},
+            'FEAT': {'div': 0.3016, 'rate': 63.89, 'sec': 79.10, 'roc': 46.75, 'name': 'Dorsey Wright Featured 5'},
+            'FIVY': {'div': 0.1895, 'rate': 32.38, 'sec': 42.30, 'roc': 64.35, 'name': 'Dorsey Wright Hybrid 5'},
+            'GPTY': {'div': 0.2915, 'rate': 35.58, 'sec': 0.00, 'roc': 23.70, 'name': 'AI & Tech Portfolio'},
+            'LFGY': {'div': 0.2822, 'rate': 54.19, 'sec': 3.47, 'roc': 0.00, 'name': 'Crypto Industry & Tech'},
+            'QDTY': {'div': 0.2605, 'rate': 31.19, 'sec': 0.23, 'roc': 63.75, 'name': 'Nasdaq 100 0DTE'},
+            'RDTY': {'div': 0.2594, 'rate': 32.80, 'sec': 1.48, 'roc': 77.09, 'name': 'R2000 0DTE'},
+            'SDTY': {'div': 0.2014, 'rate': 23.40, 'sec': 0.03, 'roc': 80.96, 'name': 'S&P 500 0DTE'},
+            'SLTY': {'div': 0.3528, 'rate': 60.71, 'sec': 1.81, 'roc': 70.44, 'name': 'Ultra Short Option'},
+            'ULTY': {'div': 0.5054, 'rate': 70.05, 'sec': 0.00, 'roc': 100.00, 'name': 'Ultra Option Strategy'},
+            'YMAG': {'div': 0.0857, 'rate': 32.01, 'sec': 64.33, 'roc': 59.40, 'name': 'Magnificent 7 Fund'},
+            'YMAX': {'div': 0.0824, 'rate': 42.65, 'sec': 94.58, 'roc': 61.09, 'name': 'Universe Fund'},
         }
     }
