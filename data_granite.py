5361b6701b7304f19173680b59af76339fda0916
gla512356 <gla512356@gmail.com>
2026-01-24 19:38:43 +0900

    Update data_granite.py

--- data_granite.py
+++ data_granite.py
@@ -3,39 +3,39 @@
 def get_data():
     return {
         "title": "GraniteShares YieldBOOST™",
-        # GraniteShares 테마 (Deep Night: Dark Blue & Dark Grey)
+        # GraniteShares Theme (Deep Night: Dark Blue & Dark Grey)
         "theme_color": ["#0F2027", "#2C5364"], 
         
         "schedule": {
-            "buy_limit": "1/16(금) 06:00", 
-            "ex_date": "1/16(금)",
-            "pay_date": "1/20(화)" 
+            "buy_limit": "1/23(금) 06:00", 
+            "ex_date": "1/23(금)",
+            "pay_date": "1/27(화)" 
         },
         
         "tickers": {
-            'NVYY': {'div': 0.3429, 'rate': 100.20, 'sec': 1.15, 'roc': 97.55, 'name': 'NVDA YieldBOOST™'},
-            'TQQY': {'div': 0.1475, 'rate': 50.52, 'sec': 1.12, 'roc': 94.45, 'name': 'QQQ YieldBOOST™'},
-            'TSYY': {'div': 0.1353, 'rate': 138.07, 'sec': 1.18, 'roc': 97.96, 'name': 'TSLA YieldBOOST™'},
-            'XBTY': {'div': 0.2069, 'rate': 97.14, 'sec': 0.99, 'roc': 97.50, 'name': 'Bitcoin YieldBOOST™'},
-            'YSPY': {'div': 0.1745, 'rate': 51.02, 'sec': 0.78, 'roc': 69.72, 'name': 'SPY YieldBOOST™'},
-            'COYY': {'div': 0.2227, 'rate': 149.71, 'sec': 1.00, 'roc': 98.31, 'name': 'COIN YieldBOOST™'},
-            'AMYY': {'div': 0.4065, 'rate': 101.92, 'sec': 0.98, 'roc': 97.86, 'name': 'AMD YieldBOOST™'},
-            'AZYY': {'div': 0.1816, 'rate': 49.39, 'sec': 0.87, 'roc': 94.70, 'name': 'AMZN YieldBOOST™'},
-            'MTYY': {'div': 0.2288, 'rate': 138.30, 'sec': 0.99, 'roc': 98.15, 'name': 'MSTR YieldBOOST™'},
-            'PLYY': {'div': 0.3379, 'rate': 97.50, 'sec': 0.69, 'roc': 97.62, 'name': 'PLTR YieldBOOST™'},
-            'SMYY': {'div': 0.3215, 'rate': 137.36, 'sec': 2.03, 'roc': 97.79, 'name': 'SMCI YieldBOOST™'},
-            'HOYY': {'div': 0.3630, 'rate': 138.45, 'sec': 1.66, 'roc': 98.00, 'name': 'HOOD YieldBOOST™'},
-            'BBYY': {'div': 0.3624, 'rate': 99.06, 'sec': 0.77, 'roc': 100.00, 'name': 'BABA YieldBOOST™'},
-            'FBYY': {'div': 0.1747, 'rate': 49.70, 'sec': 0.74, 'roc': 100.00, 'name': 'META YieldBOOST™'},
-            'IOYY': {'div': 0.5195, 'rate': 152.72, 'sec': 0.97, 'roc': 98.34, 'name': 'IONQ YieldBOOST™'},
-            'MAAY': {'div': 0.4080, 'rate': 146.92, 'sec': 0.59, 'roc': 98.44, 'name': 'MARA YieldBOOST™'},
-            'NUGY': {'div': 0.4433, 'rate': 99.67, 'sec': -0.05, 'roc': 97.96, 'name': 'Gold Miners YieldBOOST™'},
-            'SEMY': {'div': 0.5511, 'rate': 138.53, 'sec': 1.11, 'roc': 79.01, 'name': 'Semiconductor YieldBOOST™'},
-            'RGYY': {'div': 0.5374, 'rate': 143.07, 'sec': 0.28, 'roc': 98.62, 'name': 'RGTI YieldBOOST™'},
-            'QBY': {'div': 0.4764, 'rate': 136.99, 'sec': 0.33, 'roc': 98.39, 'name': 'QBTS YieldBOOST™'},
-            'RTYY': {'div': 0.5541, 'rate': 151.88, 'sec': -0.01, 'roc': 98.39, 'name': 'RIOT YieldBOOST™'},
-            'HMYY': {'div': 0.4530, 'rate': 141.05, 'sec': 1.37, 'roc': 98.05, 'name': 'HIMS YieldBOOST™'},
-            'YBTY': {'div': 0.4150, 'rate': 96.30, 'sec': 0.00, 'roc': 0.00, 'name': 'TopYielders YieldBOOST™'},
-            'YBST': {'div': 0.3583, 'rate': 80.50, 'sec': 0.00, 'roc': 0.00, 'name': 'Single Stock Universe ETF'},
+            'NVYY': {'div': 0.3452, 'rate': 100.20, 'sec': 1.15, 'roc': 97.40, 'name': 'NVDA YieldBOOST™'},
+            'TQQY': {'div': 0.1438, 'rate': 50.10, 'sec': 1.12, 'roc': 96.31, 'name': 'QQQ YieldBOOST™'},
+            'TSYY': {'div': 0.1318, 'rate': 142.58, 'sec': 1.18, 'roc': 98.41, 'name': 'TSLA YieldBOOST™'},
+            'XBTY': {'div': 0.1939, 'rate': 98.69, 'sec': 0.99, 'roc': 97.82, 'name': 'Bitcoin YieldBOOST™'},
+            'YSPY': {'div': 0.1667, 'rate': 49.68, 'sec': 0.78, 'roc': 76.57, 'name': 'SPY YieldBOOST™'},
+            'COYY': {'div': 0.1938, 'rate': 147.22, 'sec': 1.00, 'roc': 98.66, 'name': 'COIN YieldBOOST™'},
+            'AMYY': {'div': 0.4041, 'rate': 100.26, 'sec': 0.98, 'roc': 98.40, 'name': 'AMD YieldBOOST™'},
+            'AZYY': {'div': 0.1772, 'rate': 50.21, 'sec': 0.87, 'roc': 95.48, 'name': 'AMZN YieldBOOST™'},
+            'MTYY': {'div': 0.2213, 'rate': 143.96, 'sec': 0.99, 'roc': 98.79, 'name': 'MSTR YieldBOOST™'},
+            'PLYY': {'div': 0.3176, 'rate': 101.08, 'sec': 0.69, 'roc': 97.82, 'name': 'PLTR YieldBOOST™'},
+            'SMYY': {'div': 0.3417, 'rate': 137.83, 'sec': 2.03, 'roc': 98.39, 'name': 'SMCI YieldBOOST™'},
+            'HOYY': {'div': 0.3166, 'rate': 139.87, 'sec': 1.66, 'roc': 98.66, 'name': 'HOOD YieldBOOST™'},
+            'BBYY': {'div': 0.3720, 'rate': 101.65, 'sec': 0.77, 'roc': 99.80, 'name': 'BABA YieldBOOST™'},
+            'FBYY': {'div': 0.1741, 'rate': 51.45, 'sec': 0.74, 'roc': 99.08, 'name': 'META YieldBOOST™'},
+            'IOYY': {'div': 0.4770, 'rate': 146.66, 'sec': 0.97, 'roc': 99.14, 'name': 'IONQ YieldBOOST™'},
+            'MAAY': {'div': 0.3803, 'rate': 147.70, 'sec': 0.59, 'roc': 99.09, 'name': 'MARA YieldBOOST™'},
+            'NUGY': {'div': 0.4405, 'rate': 98.17, 'sec': -0.05, 'roc': 0.00, 'name': 'Gold Miners YieldBOOST™'},
+            'SEMY': {'div': 0.5593, 'rate': 138.51, 'sec': 1.11, 'roc': 0.00, 'name': 'Semiconductor YieldBOOST™'},
+            'RGYY': {'div': 0.4897, 'rate': 141.61, 'sec': 0.28, 'roc': 99.03, 'name': 'RGTI YieldBOOST™'},
+            'QBY': {'div': 0.4245, 'rate': 139.44, 'sec': 0.33, 'roc': 97.94, 'name': 'QBTS YieldBOOST™'},
+            'RTYY': {'div': 0.5245, 'rate': 147.64, 'sec': -0.01, 'roc': 99.01, 'name': 'RIOT YieldBOOST™'},
+            'HMYY': {'div': 0.3950, 'rate': 139.99, 'sec': 1.37, 'roc': 98.49, 'name': 'HIMS YieldBOOST™'},
+            'YBTY': {'div': 0.3854, 'rate': 93.65, 'sec': 0.00, 'roc': 0.00, 'name': 'TopYielders YieldBOOST™'},
+            'YBST': {'div': 0.3358, 'rate': 79.51, 'sec': 0.00, 'roc': 0.00, 'name': 'Single Stock Universe ETF'},
         }
     }
