95d9e9db83d9bb3e7c457c5712972c112bc5e98a
gla512356 <gla512356@gmail.com>
2026-02-03 20:33:41 +0900

    Update data_rex.py

--- data_rex.py
+++ data_rex.py
@@ -7,20 +7,20 @@ def get_data():
         "theme_color": ["#021B79", "#0575E6"], 
 
         "schedule": {
-            "buy_limit": "1/27(화) 06:00", 
-            "ex_date": "1/27(화)",
-            "pay_date": "1/28(수)" 
+            "buy_limit": "2/3(화) 06:00", 
+            "ex_date": "2/3(화)",
+            "pay_date": "2/4(수)" 
         },
 
         "tickers": {
-            'NVII': {'div': 0.1752, 'rate': 32.41, 'sec': 4.18, 'roc': 100.00, 'name': 'NVDA Growth & Income'},
-            'TSII': {'div': 0.1837, 'rate': 38.27, 'sec': 4.26, 'roc': 100.00, 'name': 'TSLA Growth & Income'},
-            'MSII': {'div': 0.0405, 'rate': 24.73, 'sec': 5.31, 'roc': 100.00, 'name': 'MSTR Growth & Income'},
-            'COII': {'div': 0.0545, 'rate': 20.85, 'sec': 4.98, 'roc': 100.00, 'name': 'COIN Growth & Income'},
-            'HOII': {'div': 0.0765, 'rate': 24.01, 'sec': 3.94, 'roc': 100.00, 'name': 'HOOD Growth & Income'},
-            'LLII': {'div': 0.1680, 'rate': 31.56, 'sec': 2.49, 'roc': 100.00, 'name': 'LLY Growth & Income'},
-            'CWII': {'div': 0.1192, 'rate': 41.14, 'sec': 4.96, 'roc': 100.00, 'name': 'CRWV Growth & Income'},
-            'PLTI': {'div': 0.0748, 'rate': 20.57, 'sec': 3.10, 'roc': 100.00, 'name': 'PLTR Growth & Income'},
-            'WMTI': {'div': 0.1067, 'rate': 20.12, 'sec': 2.46, 'roc': 100.00, 'name': 'WMT Growth & Income'},
+            'NVII': {'div': 0.2321, 'rate': 43.60, 'sec': 4.05, 'roc': 100.00, 'name': 'NVDA Growth & Income'},
+            'TSII': {'div': 0.3820, 'rate': 85.47, 'sec': 4.76, 'roc': 100.00, 'name': 'TSLA Growth & Income'},
+            'MSII': {'div': 0.0373, 'rate': 26.84, 'sec': 4.89, 'roc': 100.00, 'name': 'MSTR Growth & Income'},
+            'COII': {'div': 0.0491, 'rate': 22.25, 'sec': 5.55, 'roc': 100.00, 'name': 'COIN Growth & Income'},
+            'HOII': {'div': 0.0651, 'rate': 22.81, 'sec': 3.90, 'roc': 100.00, 'name': 'HOOD Growth & Income'},
+            'LLII': {'div': 0.1963, 'rate': 38.45, 'sec': 2.52, 'roc': 100.00, 'name': 'LLY Growth & Income'},
+            'CWII': {'div': 0.1023, 'rate': 34.44, 'sec': 3.21, 'roc': 100.00, 'name': 'CRWV Growth & Income'},
+            'PLTI': {'div': 0.0644, 'rate': 20.53, 'sec': 3.54, 'roc': 100.00, 'name': 'PLTR Growth & Income'},
+            'WMTI': {'div': 0.0928, 'rate': 16.85, 'sec': 2.75, 'roc': 100.00, 'name': 'WMT Growth & Income'},
         }
     }
