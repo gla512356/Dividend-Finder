e0da3aa8cb2640dcd8ec252bcc4ceb87decb966c
gla512356 <gla512356@gmail.com>
2026-02-15 20:41:06 +0900

    Update data_roundhill.py

--- data_roundhill.py
+++ data_roundhill.py
@@ -2,39 +2,39 @@
 
 def get_data():
     return {
-        "title": "Roundhill WeeklyPay™ (2월 2주차)",
+        "title": "Roundhill WeeklyPay™ (2월 3주차)",
         # Roundhill Theme Color (Deep Teal & Mint)
         "theme_color": ["#0f766e", "#14b8a6"], 
 
         "schedule": {
-            "buy_limit": "2/9(월) 06:00", 
-            "ex_date": "2/9(월)",
-            "pay_date": "2/10(화)" 
+            "buy_limit": "2/13(금) 06:00", 
+            "ex_date": "2/17(화)",
+            "pay_date": "2/18(수)" 
         },
 
         "tickers": {
-            'MSTW': {'div': 0.0886, 'rate': 80.72, 'sec': -0.66, 'roc': 100.00, 'name': 'MSTR WeeklyPay'},
-            'HOOW': {'div': 0.3278, 'rate': 65.71, 'sec': 3.49, 'roc': 100.00, 'name': 'HOOD WeeklyPay'},
-            'GDXW': {'div': 0.2603, 'rate': 22.86, 'sec': 2.16, 'roc': 100.00, 'name': 'Gold Miners Weekly'},
-            'AMDW': {'div': 0.3488, 'rate': 43.97, 'sec': 1.49, 'roc': 100.00, 'name': 'AMD WeeklyPay'},
-            'PLTW': {'div': 0.1170, 'rate': 25.26, 'sec': 2.14, 'roc': 100.00, 'name': 'PLTR WeeklyPay'},
-            'COIW': {'div': 0.1034, 'rate': 49.02, 'sec': 5.06, 'roc': 100.00, 'name': 'COIN WeeklyPay'},
-            'TSLW': {'div': 0.2054, 'rate': 39.15, 'sec': 1.50, 'roc': 100.00, 'name': 'TSLA WeeklyPay'},
-            'NVDW': {'div': 0.3854, 'rate': 55.54, 'sec': 1.81, 'roc': 100.00, 'name': 'NVDA WeeklyPay'},
-            'AVGW': {'div': 0.4987, 'rate': 65.29, 'sec': 2.27, 'roc': 100.00, 'name': 'AVGO WeeklyPay'},
-            'ARMW': {'div': 0.1549, 'rate': 31.13, 'sec': 3.08, 'roc': 100.00, 'name': 'ARM WeeklyPay'},
-            'BABW': {'div': 0.3302, 'rate': 42.45, 'sec': 2.10, 'roc': 100.00, 'name': 'BABA WeeklyPay'},
-            'UBEW': {'div': 0.2283, 'rate': 34.57, 'sec': 2.27, 'roc': 100.00, 'name': 'UBER WeeklyPay'},
-            'UNHW': {'div': 0.1169, 'rate': 16.44, 'sec': 2.92, 'roc': 100.00, 'name': 'UNH WeeklyPay'},
-            'NFLW': {'div': 0.1265, 'rate': 29.73, 'sec': 3.57, 'roc': 100.00, 'name': 'NFLX WeeklyPay'},
-            'GOOW': {'div': 0.6253, 'rate': 44.67, 'sec': 1.34, 'roc': 100.00, 'name': 'GOOGL WeeklyPay'},
-            'AMZW': {'div': 0.3035, 'rate': 40.38, 'sec': 1.87, 'roc': 100.00, 'name': 'AMZN WeeklyPay'},
-            'METW': {'div': 0.3184, 'rate': 47.26, 'sec': 2.18, 'roc': 100.00, 'name': 'META WeeklyPay'},
-            'GLDW': {'div': 0.2792, 'rate': 24.85, 'sec': 2.16, 'roc': 100.00, 'name': 'Gold WeeklyPay'},
-            'MSFW': {'div': 0.0965, 'rate': 16.82, 'sec': 2.95, 'roc': 100.00, 'name': 'MSFT WeeklyPay'},
-            'COSW': {'div': 0.1330, 'rate': 14.21, 'sec': 1.95, 'roc': 100.00, 'name': 'COST WeeklyPay'},
-            'AAPW': {'div': 0.2426, 'rate': 31.45, 'sec': 2.43, 'roc': 100.00, 'name': 'AAPL WeeklyPay'},
-            'BRKW': {'div': 0.1504, 'rate': 17.81, 'sec': 2.77, 'roc': 100.00, 'name': 'BRKB WeeklyPay'},
-            'TSYW': {'div': 0.0885, 'rate': 9.75, 'sec': 2.82, 'roc': 100.00, 'name': 'Treasury Weekly'},
+            'MSTW': {'div': 0.0909, 'rate': 61.86, 'sec': -0.66, 'roc': 100.00, 'name': 'MSTR WeeklyPay'},
+            'HOOW': {'div': 0.1745, 'rate': 33.29, 'sec': 3.49, 'roc': 100.00, 'name': 'HOOD WeeklyPay'},
+            'GDXW': {'div': 0.6914, 'rate': 53.07, 'sec': 2.16, 'roc': 100.00, 'name': 'Gold Miners Weekly'},
+            'AMDW': {'div': 0.2158, 'rate': 25.02, 'sec': 1.49, 'roc': 100.00, 'name': 'AMD WeeklyPay'},
+            'PLTW': {'div': 0.1809, 'rate': 38.74, 'sec': 2.14, 'roc': 100.00, 'name': 'PLTR WeeklyPay'},
+            'COIW': {'div': 0.0772, 'rate': 31.81, 'sec': 5.06, 'roc': 100.00, 'name': 'COIN WeeklyPay'},
+            'TSLW': {'div': 0.1925, 'rate': 34.85, 'sec': 1.50, 'roc': 100.00, 'name': 'TSLA WeeklyPay'},
+            'NVDW': {'div': 0.2909, 'rate': 39.31, 'sec': 1.81, 'roc': 100.00, 'name': 'NVDA WeeklyPay'},
+            'AVGW': {'div': 0.4495, 'rate': 56.38, 'sec': 2.27, 'roc': 100.00, 'name': 'AVGO WeeklyPay'},
+            'ARMW': {'div': 0.4128, 'rate': 72.29, 'sec': 3.08, 'roc': 100.00, 'name': 'ARM WeeklyPay'},
+            'BABW': {'div': 0.2606, 'rate': 34.34, 'sec': 2.10, 'roc': 100.00, 'name': 'BABA WeeklyPay'},
+            'UBEW': {'div': 0.1357, 'rate': 22.59, 'sec': 2.27, 'roc': 100.00, 'name': 'UBER WeeklyPay'},
+            'UNHW': {'div': 0.2121, 'rate': 26.97, 'sec': 2.92, 'roc': 100.00, 'name': 'UNH WeeklyPay'},
+            'NFLW': {'div': 0.1539, 'rate': 38.69, 'sec': 3.57, 'roc': 100.00, 'name': 'NFLX WeeklyPay'},
+            'GOOW': {'div': 0.3099, 'rate': 24.64, 'sec': 1.34, 'roc': 100.00, 'name': 'GOOGL WeeklyPay'},
+            'AMZW': {'div': 0.0838, 'rate': 12.92, 'sec': 1.87, 'roc': 100.00, 'name': 'AMZN WeeklyPay'},
+            'METW': {'div': 0.1003, 'rate': 15.91, 'sec': 2.18, 'roc': 100.00, 'name': 'META WeeklyPay'},
+            'GLDW': {'div': 0.3948, 'rate': 33.46, 'sec': 2.16, 'roc': 100.00, 'name': 'Gold WeeklyPay'},
+            'MSFW': {'div': 0.0741, 'rate': 12.67, 'sec': 2.95, 'roc': 100.00, 'name': 'MSFT WeeklyPay'},
+            'COSW': {'div': 0.2986, 'rate': 30.92, 'sec': 1.95, 'roc': 100.00, 'name': 'COST WeeklyPay'},
+            'AAPW': {'div': 0.2614, 'rate': 37.37, 'sec': 2.43, 'roc': 100.00, 'name': 'AAPL WeeklyPay'},
+            'BRKW': {'div': 0.1972, 'rate': 23.82, 'sec': 2.77, 'roc': 100.00, 'name': 'BRKB WeeklyPay'},
+            'TSYW': {'div': 0.1255, 'rate': 13.46, 'sec': 2.82, 'roc': 100.00, 'name': 'Treasury Weekly'},
         }
     }
